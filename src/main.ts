cef991425423777eb620b9bb0131fb4ddb2c75b1
Filippo <113581489+FilippoPietroNeri@users.noreply.github.com>
2026-01-26 18:31:59 +0000

    Added SERVER_PORT enviroment

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import path from 'path';
 import { startAnnoMonitor } from './monitor/annoMonitor';
 import { logger } from './utils/logger';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.SERVER_PORT || 3000;
 
 app.use(express.static(path.join(process.cwd(),'/src/api/public')));
 app.get('/api', (req, res) => {
